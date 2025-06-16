b1242152d200a8ae8b3a06afdb6b6f418d72ba17
Roman Plášil <me@rplasil.name>
2025-06-16 11:10:13 +0800

    disable delete world button

--- builtin/mainmenu/tab_local.lua
+++ builtin/mainmenu/tab_local.lua
@@ -214,7 +214,7 @@ local function get_formspec(tabview, name, tabdata)
 
 	retval = retval ..
 			"container[5.25,4.875]" ..
-			"button[0,0;3.225,0.8;world_delete;".. fgettext("Delete") .. "]" ..
+			-- "button[0,0;3.225,0.8;world_delete;".. fgettext("Delete") .. "]" ..
 			"button[3.325,0;3.225,0.8;world_configure;".. fgettext("Select Mods") .. "]" ..
 			"button[6.65,0;3.225,0.8;world_create;".. fgettext("New") .. "]" ..
 			"container_end[]" ..
