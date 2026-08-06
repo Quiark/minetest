43ecefaf40b3296defadc180b00e0e0489bc171a
Roman Plášil <me@rplasil.name>
2026-08-06 22:38:36 +0800

    fix build with nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Development shell flake replacing shell.nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          env.LANG = "C.UTF-8";
+          env.LC_ALL = "C.UTF-8";
+          packages = [
+            pkgs.gcc
+            pkgs.cmake
+            pkgs.zlib
+            pkgs.zstd
+            pkgs.libjpeg
+            pkgs.libpng
+            pkgs.libGL
+            pkgs.SDL2
+            pkgs.openal
+            pkgs.curl
+            pkgs.libvorbis
+            pkgs.libogg
+            pkgs.gettext
+            pkgs.freetype
+            pkgs.sqlite
+          ];
+        };
+      }
+    );
+}
--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {}, }:
 
 pkgs.mkShell {
-  LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+  #LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
   env.LANG = "C.UTF-8";
   env.LC_ALL = "C.UTF-8";
 
@@ -14,7 +14,7 @@ pkgs.mkShell {
     pkgs.libpng
     pkgs.libGL
     pkgs.luajit
-    pkgs.sdl3
+    # pkgs.sdl3 # needs specific version
     pkgs.openal
     pkgs.curl
     pkgs.libvorbis
